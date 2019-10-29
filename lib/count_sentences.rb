f4c6d523a82de7d39def77b4f4511f4004cd27cd
github username <github email address>
2019-10-29 16:07:12 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return true if self[-1] == "."
+    return false
   end
 
   def question?
-
+    return true if self[-1] == "?"
+    return false
   end
 
   def exclamation?
-
+    return true if self[-1] == "!"
+    return false
   end
 
   def count_sentences
-
+    sentence_delimiters = [".","?","!"]
+    sentence_count = 0
+    
+    self.chars.each_with_index do |character, i|
+      if sentence_delimiters.include?(character) && !sentence_delimiters.include?(self.chars[i - 1])
+        sentence_count += 1
+      end
+    end
+    sentence_count
   end
+
 end
+
